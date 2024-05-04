9cbbb29cd3eac0130a358fb2484e96f13a688c5f
mashaojie <mashaojie@pinefield-inc.com>
2024-05-04 15:55:34 +0800

    feat: 项目代码初始化

---
+++ pages/index/index.vue
@@ -0,0 +1,86 @@
+<template>
+	<view class="content">
+		<button @click="getOpenId">获取openid</button>
+		<button @click="getAccessToken">获取access_token</button>
+	</view>
+</template>
+
+<script>
+	const WXMP_URL = 'https://www.mashaojie.cn/weixin'
+	export default {
+		data() {
+			return {
+				title: '功能测试项目',
+				provider: '',
+				code: '',
+			}
+		},
+		methods: {
+			async getProvider() {
+				try{
+					const getProviderResp = await uni.getProvider({service: 'oauth'})
+					this.provider = getProviderResp.provider[0]
+					return this.provider
+				}catch(e){
+					console.error(e)
+				}
+			},
+			async getCode() {
+				try{
+					const provider = await this.getProvider()
+					const loginRes = await uni.login({provider: provider})
+					if(loginRes.errMsg === "login:ok") {
+						this.code = loginRes.code
+						return this.code
+					} else {
+						console.error(loginRes)
+					}
+				}catch(e){
+					console.error(e)
+				}
+			},
+			async getOpenId() {
+				try{
+					await this.getCode()
+					const resp = await uni.request({
+						url: WXMP_URL + '/wxmpApi/getOpenidAndToken',
+						method: 'POST',
+						data: {
+							code: this.code
+						}
+					})
+					
+					console.log(resp)
+				}catch(e){
+					console.error(e)
+				}
+			},
+			async getAccessToken() {
+				try{
+					const resp = await uni.request({
+						url: WXMP_URL + '/wxmpApi/getAccessToken',
+						method: 'GET'
+					})
+					
+					console.log(resp)
+				}catch(e){
+					console.error(e)
+				}
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		
+		button {
+			margin: 20rpx;
+			width: 70%;
+		}
+	}
+</style>
---
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -0,0 +1,71 @@
+"use strict";
+const common_vendor = require("../../common/vendor.js");
+const WXMP_URL = "https://www.mashaojie.cn/weixin";
+const _sfc_main = {
+  data() {
+    return {
+      title: "功能测试项目",
+      provider: "",
+      code: ""
+    };
+  },
+  methods: {
+    async getProvider() {
+      try {
+        const getProviderResp = await common_vendor.index.getProvider({ service: "oauth" });
+        this.provider = getProviderResp.provider[0];
+        return this.provider;
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    async getCode() {
+      try {
+        const provider = await this.getProvider();
+        const loginRes = await common_vendor.index.login({ provider });
+        if (loginRes.errMsg === "login:ok") {
+          this.code = loginRes.code;
+          return this.code;
+        } else {
+          console.error(loginRes);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    async getOpenId() {
+      try {
+        await this.getCode();
+        const resp = await common_vendor.index.request({
+          url: WXMP_URL + "/wxmpApi/getOpenidAndToken",
+          method: "POST",
+          data: {
+            code: this.code
+          }
+        });
+        console.log(resp);
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    async getAccessToken() {
+      try {
+        const resp = await common_vendor.index.request({
+          url: WXMP_URL + "/wxmpApi/getAccessToken",
+          method: "GET"
+        });
+        console.log(resp);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }
+};
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return {
+    a: common_vendor.o((...args) => $options.getOpenId && $options.getOpenId(...args)),
+    b: common_vendor.o((...args) => $options.getAccessToken && $options.getAccessToken(...args))
+  };
+}
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "/Users/jetsommax/Documents/HBuilderProjects/weixin-test-project/pages/index/index.vue"]]);
+wx.createPage(MiniProgramPage);
